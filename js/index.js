e9307f496e7f82d69aa3829a3dee7a6c1415891b
Christian Charbonnier <christian.charbonnier@estudiantes.utec.edu.uy>
2025-09-03 15:23:11 -0300

    resultado

--- js/index.js
+++ js/index.js
@@ -1,16 +1,4 @@
-// Accedemos al valor asociado a "example"
-// que devolverá null (nulo) ya que no hay nada cargado
-console.log(localStorage.getItem("example"));
-
-// Guardamos un valor cualquiera asociado a la clave "example"
-// (notar que siempre debe almacenarse en String)
-localStorage.setItem("example", "example value");
-
-// Mostramos que ahora sí hay un valor asociado
-console.log(localStorage.getItem("example"));
-
-// Eliminamos el elemento asociado a "example"
-localStorage.removeItem("example");
-
-// Vemos que nuevamente es null
-console.log(localStorage.getItem("example"));
+document.getElementById('buttonText').addEventListener('click', function() {
+    let texto = document.getElementById('inputText').value;
+    localStorage.setItem('text', texto);
+});
